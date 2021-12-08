95d09fa8b84cf7cb1eb12793cf4d153dccc5c6fd
varsill <lukasz.kita0@gmail.com>
2021-12-08 14:38:19 +0100

    Fix the version of membrane_rtc_engine

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule VideoRoom.MixProject do
   defp deps do
     [
       {:membrane_core, github: "membraneframework/membrane_core", override: true},
-      {:membrane_rtc_engine, github: "membraneframework/membrane_rtc_engine"},
+      {:membrane_rtc_engine, github: "membraneframework/membrane_rtc_engine", ref: "c9e03b3d6457fe3b34fafc93fe8407a1b160e36a"},
       {:esbuild, "~> 0.1", runtime: Mix.env() == :dev},
       {:plug_cowboy, "~> 2.0"},
       {:phoenix, "~> 1.6"},
