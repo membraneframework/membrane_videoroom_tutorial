1b64b76aa7e4f7a28ddc97994648172f8e9c20b2
Piotrek Zyla <piotr.zyla@swmansion.com>
2021-12-10 17:03:01 +0100

    End of tutorial

--- lib/videoroom_web/peer_channel.ex
+++ lib/videoroom_web/peer_channel.ex
@@ -3,4 +3,40 @@ defmodule VideoRoomWeb.PeerChannel do
 
   require Logger
 
+  @impl true
+  def join("room:" <> room_id, _params, socket) do
+    case :global.whereis_name(room_id) do
+       :undefined -> Videoroom.Room.start(name: {:global, room_id})
+       pid -> {:ok, pid}
+    end
+    |> case do
+       {:ok, room} ->
+       peer_id = "#{UUID.uuid4()}"
+       Process.monitor(room)
+       send(room, {:add_peer_channel, self(), peer_id})
+       {:ok, Phoenix.Socket.assign(socket, %{room_id: room_id, room: room, peer_id: peer_id})}
+
+       {:error, reason} ->
+       Logger.error("""
+       Failed to start room.
+       Room: #{inspect(room_id)}
+       Reason: #{inspect(reason)}
+       """)
+
+       {:error, %{reason: "failed to start room"}}
+    end
+  end
+
+  @impl true
+  def handle_info({:media_event, event}, socket) do
+    push(socket, "mediaEvent", %{data: event})
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_in("mediaEvent", %{"data" => event}, socket) do
+   send(socket.assigns.room, {:media_event, socket.assigns.peer_id, event})
+   {:noreply, socket}
+  end
+
 end
